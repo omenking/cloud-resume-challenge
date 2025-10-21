feb9c8a261785ac5fb54b269001e9328538af59f
Andrew Brown <andrew@monsterboxpro.com>
2025-10-21 15:19:55 +0000

    implement view counter and mock api

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,19 +4,20 @@ import andrew_brown from 'images/andrew-brown-thumb.webp'
 import blogData from 'data/blogData.json'
 import linksData from 'data/linksData.json'
 import PostItem from 'comps/PostItem'
-
+import ViewCounter from 'comps/ViewCounter'
 export default function HomePage() {
   const sortedBlogData = [...blogData].sort((a, b) => new Date(b.date) - new Date(a.date));
 
   return (
     <>
       <h1 className='fancy'>Andrew Brown's Blog</h1>
+      <ViewCounter />
       <div className="intro_video">
         <img src={andrew_brown} />
       </div>
       <div className="links">
         {linksData.map((link) => (
-          <a target="_blank" href={link.url}>
+          <a key={link.url} target="_blank" href={link.url}>
             <span className="icon" dangerouslySetInnerHTML={{ __html: link.icon }} />
             <span className="name">{link.name}</span>
           </a>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,32 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 
+const mock_server = {
+  name: "mock-counter",
+  configureServer(server) {
+    let count = 0; // in-memory mock state
+
+    server.middlewares.use("/counter", async (req, res, next) => {
+      res.setHeader("Content-Type", "application/json");
+
+      if (req.method === "GET") {
+        res.end(JSON.stringify({ count }));
+        return;
+      }
+
+      if (req.method === "POST") {
+        count += 1;
+        res.end(JSON.stringify({ count }));
+        return;
+      }
+
+      next();
+    });
+  },
+}
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),mock_server],
   resolve: {
     alias: {
       "data": path.resolve(__dirname, "src/data"),
